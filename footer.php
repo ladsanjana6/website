14c333e0b4778f28a69548cf7b13624624ca126f
ladsanjana6 <149415163+ladsanjana6@users.noreply.github.com>
2025-01-03 19:54:24 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="style.css">
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+    </head>
+
+    <body>
+        <!-- Footer starts -->
+        <footer>
+            <div class="container">
+                <div class="footer1">
+                    <div class="row">
+                        <div class="col">
+                            <div class="icon">
+                                <i class="bi bi-geo-alt-fill"></i>
+                                <div class="txt">
+                                    <h4>Find Us</h4>
+                                    <span>
+                                        908, Signature Business Park, K. A. Gaikwad Marg, opp. Golden Lawn Restaurant,
+                                        Chembur East, Mumbai - 400071. India
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="icon">
+                                <i class="bi bi-telephone-fill"></i>
+                                <div class="txt">
+                                    <h4>Call Us</h4>
+                                    <span>
+                                        +91- 22 252 90000 / 9820083569
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="icon">
+                                <i class="bi bi-envelope-open"></i>
+                                <div class="txt">
+                                    <h4>Mail Us</h4>
+                                    <span>
+                                        business@vaba.co.in
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer2">
+                    <div class="row">
+                        <div class="col">
+                            <div class="p1">
+                                <div class="logo">
+                                    <a href="index.html">
+                                        <img src="https://www.vaba.co.in/images/logo.png" alt="">
+                                    </a>
+                                </div>
+                                <div class="foot-txt">
+                                    <p>
+                                        We provide structural concrete form work engineering and form work design
+                                        services for all kinds ofbig and small scale construction projects through out
+                                        the India.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="p1">
+                                <div class="p2">
+                                    <h3>Quick Links</h3>
+                                </div>
+                                <ul>
+                                    <li>
+                                        <a href="index.html" class="scroll">
+                                            Home
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="about.html" class="scroll">
+                                            About Us
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="products.html" class="scroll">
+                                            Products
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="Image.html" class="scroll">
+                                            Images
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="Videos.html" class="scroll">
+                                            Videos
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.vaba.co.in/vaba-new-broucher.pdf" class="scroll"
+                                            target='_blank'>
+                                            E-Catalogue
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="Contact.html" class="scroll">
+                                            Contact Us
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="p1">
+                                <div class="p2">
+                                    <h3>Folow Us</h3>
+                                </div>
+                                <div class="footer-social-icon">
+                                    <a href="https://www.facebook.com/vabaaluminiumformwork/" traget="_blank">
+                                        <img src="Facebook.png" alt="Facebook">
+                                    </a>
+                                    <a href="https://www.instagram.com/vabaaluminiumformwork/" traget="_blank">
+                                        <img src="Instagram.png" alt="Instagram">
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/tony-bansal-746120251" traget="_blank">
+                                        <img src="Linkedin.png" alt="Linkedin">
+                                    </a>
+                                    <a href="https://www.youtube.com/@VABAALUMINIUMFORMWORK" traget="_blank">
+                                        <img src="Youtube.png" alt="Youtube">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer3">
+                    <div class="row">
+                        <div class="col">
+                            <div class="copyright">
+                                <p>
+                                    <span>Related Search:</span>
+                                    <a href="aluminium-formwork-system-manufacturers-in-india.php">Aluminium Formwork
+                                        System Manufacturers & Suppliers in India</a> |
+                                    <a href="aluminium-formwork-manufacturers-&-suppliers-in-mumbai.php">Aluminium
+                                        Formwork Manufacturers & Suppliers in Mumbai</a> |
+                                    <a href="aluminium-formwork-manufacturers-&-suppliers-in-pune.php">Aluminium
+                                        Formwork Panels Manufacturers & Suppliers in Pune</a> |
+                                    <a href="aluminium-formwork-manufacturers-in-ahmednagar.php">Aluminium Formwork
+                                        Manufacturers in Ahmednagar</a> |
+                                    <a href="aluminium-formwork-manufacturers-in-aurangabad.php">Aluminium Formwork
+                                        Manufacturers in Aurangabad</a> |
+                                    <a href="aluminium-formwork-manufacturers-in-kolhapur.php">Aluminium Formwork
+                                        Manufacturers in Kolhapur</a> |
+                                    <a href="aluminium-formwork-manufacturers-in-nagpur.php">Aluminium Formwork
+                                        Manufacturers in Nagpur</a> |
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="copyright-txt">
+                                <p>
+                                    Copyright © 2023 VABA Aluminium All Rights Reserved
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer ends -->
+    </body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="style.css">
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+    </head>
+
+    <body>
+        <!-- Whatsapp Logo starts -->
+        <div class="wtapp">
+            <img src="download.png" alt="Whatsapp Image">
+        </div>
+        <!-- Whatsapp Logo ends -->
+
+        <!-- Header starts -->
+        <header>
+            <div class="left">
+                <div class='l1'>
+                    <i class="bi bi-phone"></i>
+                    <p>+91 9820083569</p>
+                </div>
+                <div class='l1'>
+                    <i class="bi bi-envelope"></i>
+                    <p>business@vaba.co.in</p>
+                </div>
+            </div>
+            <div class="right">
+                <div>
+                    <i class="bi bi-bicebook"></i>
+                </div>
+                <div>
+                    <i class="bi bi-instagram"></i>
+                </div>
+                <div>
+                    <i class="bi bi-linkedin"></i>
+                </div>
+                <div>
+                    <i class="bi bi-youtube"></i>
+                </div>
+            </div>
+        </header>
+        <!-- Header ends -->
+
+    </body>
+
+</html>
